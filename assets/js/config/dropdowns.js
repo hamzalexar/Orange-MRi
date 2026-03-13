f0591d65fe12cc0908dd90b009a42cf7ab31081a
hamzalexar <hamza.hannach@student.pxl.be>
2026-03-13 13:07:45 +0100

    Update dropdowns.js

--- assets/js/config/dropdowns.js
+++ assets/js/config/dropdowns.js
@@ -1,6 +1,6 @@
 export const DROPDOWNS = {
     interaction: ["Outbound", "Inbound", "Mail", "Other","Escalmail"],
     contactType: ["","Customer", "Technician", "Partner","Front","Fiber","Retention","Mediation","Mail","Other"],
-    outcome: ["","Call OK", "No answer , RINGRING send", "Callback needed", "Tech planned", "Escalated", "Other","Customer confirms solved",],
+    outcome: ["","Call OK", "No answer , RINGRING send", "Callback needed", "Tech planned", "Escalated", "Other","Customer confirms solved","transferd custumer","consult call",],
   };
   
